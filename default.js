bf24656ee9e8ac8a2115eec5b88c69a3301c0313
rzparvia <arno.p.parviainen@gmail.com>
2018-11-01 11:05:43 +0200

    Torstaiaamun frontti: Viestien luonti, luuppaus ja laitto sivulle

--- default.js
+++ default.js
@@ -1,17 +1,43 @@
-var kalpa;
+var hifkviestit;
+var kalpaviestit;
+var tapparaviestit;
+
+//metodikutsust
 haeViestit();
+
+//funktiot
 function haeViestit() {
     $.getJSON('http://localhost:8080/viestit')
         .done(function (data) {
-            console.log(data)
+            console.log(data);
             console.log(data[0].topics.content);
             console.log(data[0].content);
-            //kalpa.push(data[0].topics.content);
-
+            //Kalpan viestit tällä luupilla
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].topics.title === "Kalpa") {
+                    console.log(data[i].content);
+                }
+            }
+            //HIFKin viestit tällä luupilla
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].topics.title === "HIFK") {
+                    console.log(data[i].content);
+                }
+            }
+            //Tapparan viestit tällä luupilla
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].topics.title === "Tappara") {
+                    console.log(data[i].content);
+                }
+            }
             $("<ul>").attr("id", "toka").appendTo("#testi");
-            $("<li>").text(data[0].topics.content).appendTo("#toka")
+            $("<li>").text(data[0].topics.content).appendTo("#toka");
             $("<ul>").attr("id", "jotain").appendTo("#hifkviestit");
-            $("<li>").text(data[0].content).appendTo("#jotain")
+            $("<li>").text(data[0].content).appendTo("#jotain");
+
+            //HIFKIN viestit tämän alle
+
+
         })
 }
 
